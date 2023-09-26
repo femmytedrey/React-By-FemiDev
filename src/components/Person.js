8510e2282fa9769af5717d2a0c7a0905660213a8
femmytedrey <femmytedrey@gmail.com>
2023-09-26 23:49:06 +0100

    working

--- src/components/Person.js
+++ src/components/Person.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Person({person}) {
   return (
     <div>
-      <h2>I am {person.name}. I am kg{person.age}. I know {person.skill}.</h2>
+      <h2>I am {person.name}. I am {person.age}. I know {person.skill}.</h2>
     </div>
   )
 }
