bc827965d096d0201d6f3f9fdafddca45fd477d5
femmytedrey <femmytedrey@gmail.com>
2023-09-28 12:46:19 +0100

    Form handling event

--- src/App.js
+++ src/App.js
@@ -13,18 +13,20 @@ import ParentComponent from './ParentComponent';
 import UserGreeting from './components/UserGreeting';
 import NameList from './components/NameList';
 import StyleSheet from './components/StyleSheet';
-import Inline from './components/Inline';
+// import Inline from './components/Inline';
 import './appStyles.css'
 import styles from './appStyles.module.css'
+import Form from './components/Form';
 
 class App extends Component{
   render() {
     return (
       <div className = "App">
-        <h1 className = 'error'>Error</h1>
-        <h1 className = {styles.success}>Success</h1>
+        <Form />
+        {/* <h1 className = 'error'>Error</h1>
+        <h1 className = {styles.success}>Success</h1> */}
         {/* <StyleSheet primary = {true}></StyleSheet> */}
-        <Inline />
+        {/* <Inline /> */}
         {/* <NameList /> */}
         {/* <UserGreeting /> */}
         {/* <ParentComponent /> */}
