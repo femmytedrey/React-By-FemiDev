277ce9c8370410924398c7dac7442b4a8793a41f
femmytedrey <femmytedrey@gmail.com>
2023-09-28 13:36:07 +0100

    working

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,7 +5,8 @@ class Form extends Component {
       super(props)
     
       this.state = {
-         username: ''
+         username: '',
+         comment: ''
       }
     }
 
@@ -15,12 +16,23 @@ class Form extends Component {
         })
     }
 
+    handleCommentChange = (event) =>{
+        this.setState({
+            comment: event.target.value
+        })
+    }
+
   render() {
     return (
         <form>
             <div>
                 <label id = "username">Username</label>
                 <input type = "text" id = "username" name = "username" value = {this.state.username} onChange={this.handleUsernameChange}/>
+            </div>
+
+            <div>
+                <label id = 'comment'>Comment</label>
+                <textarea value = {this.state.value} onChange={this.state.handleCommentChange} />
             </div>
         </form>
     )
