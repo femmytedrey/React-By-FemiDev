5bf8fcd045bf7f469d2808830586ac908e59d188
femmytedrey <femmytedrey@gmail.com>
2023-09-30 19:18:58 +0100

    working

--- src/App.js
+++ src/App.js
@@ -26,6 +26,8 @@ import RefsDemo from './components/RefsDemo';
 import FocusInput from './components/FocusInput';
 import FRParentInput from './components/FRParentInput';
 import PortalDemo from './components/PortalDemo';
+import Hero from './components/Hero';
+import ErrorBoundary from './components/ErrorBoundary';
 
 class App extends Component{
   constructor(props) {
@@ -36,7 +38,18 @@ class App extends Component{
   render() {
     return (
       <div className = "App">
-        <PortalDemo />
+        <ErrorBoundary>
+          <Hero heroName="Bat-Man"/>
+        </ErrorBoundary>
+
+        <ErrorBoundary>
+          <Hero heroName="Super-Man"/>
+        </ErrorBoundary>
+
+        <ErrorBoundary>
+          <Hero heroName="Joker"/>
+        </ErrorBoundary>        
+        {/* <PortalDemo /> */}
         {/* <FRParentInput /> */}
         {/* <FocusInput /> */}
         {/* <RefsDemo /> */}
